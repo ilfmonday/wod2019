edb5c1cfda623b31a7eea71820df08ad21881886
ilfmonday <ilfmonday@qq.com>
2019-03-15 19:43:12 +0800

    up

--- changeHttps.sh
+++ changeHttps.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
+# set -x
+
+CUR_PATH=`pwd`
+PYTHON_CMD='python mergeIndexsHtml.py'
+
+cd greenday
+echo `pwd`
+python mergeIndexsHtml.py
+cd $CUR_PATH
+
+cd buganGaoshengYu
+python mergeIndexsHtml.py
+cd $CUR_PATH
+
+cd congcijunwang
+python mergeIndexsHtml.py
+cd $CUR_PATH
+
 grep -rl "http:\/\/" . | xargs sed -i "" 's/http:\/\//https:\/\//g'
